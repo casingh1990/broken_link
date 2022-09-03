2bb65b4f440e6b268b1fe5a810f4b2fefb0ceaaa
Amit Singh <chandrashekar.singh@fulcrumdigital.com>
2022-09-03 17:37:43 -0400

    change to vue

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Link;
+use Illuminate\Http\Request;
+
+class LinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Link::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Link::unguard();
+        $link = new Link([
+            'name' => $request->input('name'),
+            'link' => $request->input('link'),
+            'status' => $request->input('status'),
+        ]);
+
+        $link->save();
+
+        return $link;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Link $link)
+    {
+        $link->update([
+            'name' => $request->input('name'),
+            'link' => $request->input('link'),
+            'status' => $request->input('status'),
+        ]);
+
+        return $link->fresh();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
